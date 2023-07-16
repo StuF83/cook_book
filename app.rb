95485aeb7d4927512bcef0acca39a3a048abb9bb
Stuart <108226770+StuF83@users.noreply.github.com>
2023-07-16 09:49:32 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: 'db/development.sqlite3'}
+
+
+get '/' do
+  "Hello World"
+end
